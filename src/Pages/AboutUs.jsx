eb3ef58757a4c62126d4277a046cc68fb2ed17d7
tsega7664 <yeabsira.zelalem@aastustudent.edu.et>
2024-06-21 16:45:25 +0300

    modifying about page

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -20,12 +20,12 @@ const AboutUs = () => {
           <p className=''>Askal Charity Society is a non-governmental indigenous organization established on 2019 to provide universal support to children. Askal is a charitable organization that has been registered and accredited by the Civil Society Organizations Authority of the Federal Democratic Republic of Ethiopia under registration number 4015.</p>
           <p className=''>The name Askal comes from a Ge'ez word meaning "fruit," which reflects the organization's purpose of cultivating a fruitful generation of model citizens who can positively shape the future of our country.<br /> Established in 2011 on the 7th of Meskerem by executive director Belai Tinsae, Askal began by permanently supporting 150 children, a number that has steadily grown over the years as the organization has expanded. By nurturing these young people, Askal aims to help develop a cohort of productive, influential individuals who will go on to serve their people and nation with dedication.</p>
         </div>
-        <div className='flex flex-col md:grid md:grid-cols-3 gap-4'>
-    <div className='p-4 text-center transform transition-transform duration-300 hover:-translate-y-2'>
-        <div className='rounded w-40 mx-auto'>
+        <div className='flex flex-col md:grid md:grid-cols-3 gap-4 md:mx-20'>
+    <div className='p-4  text-center transform transition-transform duration-300 hover:-translate-y-2'>
+        <div className='rounded w-40 mx-auto '>
             <img src={family} alt="Family" className='w-full rounded-full' />
         </div>
-        <div className='font-bold text-xl py-2'>Who We Serve</div>
+        <div className='font-bold text-xl py-4'>Who We Serve</div>
         <div className='font-bold'>
             <ul>
                 <li>Children</li>
@@ -37,7 +37,7 @@ const AboutUs = () => {
         <div className='rounded w-40 mx-auto'>
             <img src={vision} alt="Vision" className='w-full' />
         </div>
-        <div className='font-bold text-xl py-2'>Our Vision</div>
+        <div className='font-bold text-xl py-4'>Our Vision</div>
         <div className='font-bold p-2'>
             <p>Seeing all children being cared for physically, nurtured intellectually, and developed emotionally, contributing to the nation sustainably.</p>
         </div>
@@ -46,7 +46,7 @@ const AboutUs = () => {
         <div className='rounded w-40 mx-auto'>
             <img src={mission} alt="Mission" className='w-full rounded-full' />
         </div>
-        <div className='font-bold text-xl py-2'>Our Mission</div>
+        <div className='font-bold text-xl py-4'>Our Mission</div>
         <div className='font-bold p-2'>
             <p>Ensuring that all-encompassing support reaches children sustainably by expanding effective systems and filling gaps in child support, while being there for the children.</p>
         </div>
@@ -55,7 +55,7 @@ const AboutUs = () => {
         <div className='rounded w-40 mx-auto'>
             <img src={friend} alt="Friend" className='w-full rounded-full' />
         </div>
-        <div className='font-bold text-xl py-2 text-center'>How to Join Us</div>
+        <div className='font-bold text-xl py-4 text-center'>How to Join Us</div>
         <div className='font-bold p-2'>
             <ul className='text-center'>
                 <li>By becoming a permanent member</li>
@@ -69,7 +69,7 @@ const AboutUs = () => {
         <div className='rounded w-40 mx-auto'>
             <img src={recourse} alt="Resource" className='w-full rounded-full' />
         </div>
-        <div className='font-bold text-xl py-2'>Our Resources</div>
+        <div className='font-bold text-xl py-4'>Our Resources</div>
         <div className='font-bold p-2'>
             <p>To achieve our organization's goals, even if we find individual supporters who are few and far between, instead of relying solely on guardianship, we will sustain ourselves by adopting various income-generating methods.</p>
         </div>
@@ -78,7 +78,7 @@ const AboutUs = () => {
         <div className='rounded w-40 mx-auto'>
             <img src={location} alt="Resource" className='w-full rounded-full' />
         </div>
-        <div className='font-bold text-xl py-2'>Our Location</div>
+        <div className='font-bold text-xl py-4'>Our Location</div>
         <div className='font-bold p-2'>
             <p>Tekle Haymanot Building, 4th floor, Kadisco (In between Gotera & Adey abeba), Addis Ababa</p>
         </div>
