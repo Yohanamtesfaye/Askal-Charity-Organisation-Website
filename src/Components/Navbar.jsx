12833f51ca743ff89b44221399ac73feec6ebab9
tsega7664 <yeabsira.zelalem@aastustudent.edu.et>
2024-06-24 23:07:27 +0300

    NavBAr, ContactUS, Footer, AboutUs

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,27 +2,80 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../assets/askallogo.jpg';
 import { AiOutlineMenu } from 'react-icons/ai';
+
 const Navbar = () => {
-  const[nav,setNav] = useState(false);
-  const handleNav = () =>{
+  const [nav, setNav] = useState(false);
+  
+  const handleNav = () => {
     setNav(!nav);
-    }
+  }
+
   return (
-    <div className='flex justify-between px-8 pt- bg-white shadow-lg rounded-lg'>
-        <div>
-          <img src={logo} width={'70px'} alt="" />
+    <div className='font-meri'>
+      <div>
+        <AiOutlineMenu size={24} onClick={handleNav} className="absolute top-4 right-4 z-[99] md:hidden text-red-600"/>
+        <div className='pl-4 sm:hidden'>
+          <img src={logo} width={'50px'} alt="logo" />
         </div>
-        <div className='flex justify-center items-center space-x-8'>
-          <div className='hover:underline hover:text-red-600 font-bold p-0'><Link to='/' >Home</Link></div> 
-          <div className='hover:underline hover:text-red-600 font-bold p-0'><Link to='/aboutus'  >About Us</Link></div>
-          <div className='hover:underline hover:text-red-600 font-bold'> <Link to='/gallery' >Gallery</Link></div>
-          <div className='hover:underline hover:text-red-600 font-bold '> <Link to='/contactus'  >Contact Us</Link></div>
-          <div className='border-red-600 border text-red-500 font-bold '><Link to='/' ><div className=' py-2 px-10 hover:bg-red-500 hover:text-white '>Become A Familiy</div></Link></div> 
-          <div className='border bg-green-600 text-white  py-4 px-8 font-bold rounded-md hover:border-green-600 hover:bg-white hover:text-green-600 '><Link to='/donate' className=''>Donate</Link></div>
+        {
+          nav ?
+          <div>
+            <div onClick={handleNav} className="fixed w-full flex flex-col justify-center items-center bg-white">
+              <div className='shadow-xl p-4 rounded-2xl w-56 m-2 text-center'>
+                <Link to='/'> <span className="hover:underline text-red-500 font-bold ">Home</span></Link>
+              </div>
+              <div className='shadow-xl p-4 rounded-2xl w-56 m-2 text-center'>
+                <Link to='/aboutus' onClick={handleNav} ><span className="hover:underline text-red-500 font-bold  ">About Us</span></Link>
+              </div>
+              <div className='shadow-xl p-4 rounded-2xl w-56 m-2 text-center'>
+                <Link to='/gallery' onClick={handleNav}  ><span className="hover:underline text-red-500 font-bold ">Gallery</span></Link>
+              </div>
+              <div className='shadow-xl p-4 rounded-2xl w-56 m-2 text-center'>
+                <Link to='/contactus'  onClick={handleNav}><span className="hover:underline text-red-500 font-bold ">Contact Us</span></Link>
+              </div>
+              <div className="relative group">
+                <div className='shadow-xl p-4 rounded-2xl w-56 m-2 text-center'>
+                  <div><span className="hover:underline text-red-500 font-bold ">Become a Family</span></div>
+                  <div className="absolute bg-white w-full text-center text-red-500 shadow-lg rounded-md py-2 px-4 hidden group-hover:block">
+                    <ul>
+                      <li><Link to='/registervolunteer' onClick={handleNav} className='hover:underline'>Volunteer</Link></li>
+                      <li><Link to='/registermember' onClick={handleNav} className='hover:underline'>Member</Link></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div className='shadow-xl p-4 rounded-2xl w-56 m-2 text-center'>
+                <Link to='/donate' onClick={handleNav} ><span className="hover:underline text-red-500 font-bold ">Donate</span></Link>
+              </div>
+            </div>
+          </div> : ''
+        }
+        <div className='hidden md:flex justify-between px-8 bg-white shadow-lg rounded-lg'>
+          <div>
+            <img src={logo} width={'70px'} alt="logo" />
+          </div>
+          <div className='flex justify-center items-center space-x-8'>
+            <div className='hover:underline hover:text-red-600 font-bold p-0'><Link to='/' >Home</Link></div>
+            <div className='hover:underline hover:text-red-600 font-bold p-0'><Link to='/aboutus'  >About Us</Link></div>
+            <div className='hover:underline hover:text-red-600 font-bold'> <Link to='/gallery' >Gallery</Link></div>
+            <div className='hover:underline hover:text-red-600 font-bold '> <Link to='/contactus'  >Contact Us</Link></div>
+            <div className="relative group">
+              <div className='border-red-600 border text-red-500 font-bold '>
+                <div className='py-2 px-10 hover:bg-red-500 hover:text-white '>Become A Family</div>
+                <div className="absolute bg-white w-full text-center text-red-500 shadow-lg rounded-md py-2 px-4 hidden group-hover:block">
+                  <ul>
+                    <li><Link to='/registervolunteer' className='hover:underline'>Volunteer</Link></li>
+                    <li><Link to='/registermember' className='hover:underline'>Member</Link></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className='border bg-green-600 text-white py-4 px-8 font-bold rounded-md hover:border-green-600 hover:bg-white hover:text-green-600 '><Link to='/donate'>Donate</Link></div>
+          </div>
         </div>
-      
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/Pages/ContactUs.jsx
+++ src/Pages/ContactUs.jsx
@@ -1,9 +1,132 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { MdEmail } from 'react-icons/md';
+import { FaPhone, FaMapMarkerAlt, FaClock } from 'react-icons/fa';
 
 const ContactUs = () => {
+  const [formData, setFormData] = useState({
+    fullName: '',
+    phoneNumber: '',
+    email: '',
+    message: ''
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission logic
+    console.log('Form submitted:', formData);
+  };
+
   return (
-    <div>ContactUs</div>
-  )
+    <div className='font-meri '>
+      <div className='bg-customGreen text-white p-4  text-center font-bold text-5xl'> <h1>Contact Us </h1></div>
+      <div className='lg:flex lg:m-12 shadow-2xl rounded-2xl'>
+      <div className='bg-customGreen text-white lg:p-8  my-3 px-2 lg:w-1/2 rounded-2xl'>
+        <h1 className='text-4xl font-bold mb-8'>Get in Touch</h1>
+        <div className='space-y-6'>
+          <div className='flex items-center space-x-4'>
+            <MdEmail className='text-3xl' />
+            <div>
+              <p className='text-xl font-bold pt-2'>Email us</p>
+              <p className='text-yellow-300'>askalcharityassociation@gmail.com</p>
+            </div>
+          </div>
+          <div className='flex items-center space-x-4'>
+            <FaMapMarkerAlt className='text-3xl' />
+            <div>
+              <p className='text-xl font-bold pt-2'>Address</p>
+              <p>
+                Tekle Haymanot Building, 4th floor, Kadisco (In between Gotera & Adey abeba), Addis Ababa
+              </p>
+            </div>
+          </div>
+          <div className='flex items-center space-x-4'>
+            <FaPhone className='text-3xl' />
+            <div>
+              <p className='text-xl font-bold pt-2'>Call us</p>
+              <p className='text-yellow-300'>+251 90 240 4444</p>
+            </div>
+          </div>
+          <div className='flex items-center space-x-4'>
+            <FaClock className='text-3xl' />
+            <div>
+              <p className='text-xl font-bold pt-2'>Office hours</p>
+              <p>Monday to Sunday, 9:00 A.M. to 7:00 P.M.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='bg-white lg:p-8 p-4  lg:w-1/2'>
+        <form onSubmit={handleSubmit} className='space-y-4'>
+          <div>
+            <label htmlFor='fullName' className='block text-lg font-medium text-gray-700'>Full Name</label>
+            <input
+              type='text'
+              id='fullName'
+              name='fullName'
+              value={formData.fullName}
+              onChange={handleChange}
+              className='mt-1 p-2 border border-gray-300 rounded-md w-full bg-gray-200'
+              placeholder='Enter your full name'
+              
+            />
+          </div>
+          <div>
+            <label htmlFor='phoneNumber' className='block text-lg font-medium text-gray-700'>Phone Number</label>
+            <input
+              type='tel'
+              id='phoneNumber'
+              name='phoneNumber'
+              value={formData.phoneNumber}
+              onChange={handleChange}
+              className='mt-1 p-2 border border-gray-300 rounded-md w-full bg-gray-200'
+              placeholder='Enter a valid phone number'
+            />
+          </div>
+          <div>
+            <label htmlFor='email' className='block text-lg font-medium text-gray-700'>Email Address</label>
+            <input
+              type='email'
+              id='email'
+              name='email'
+              value={formData.email}
+              onChange={handleChange}
+              className='mt-1 p-2 border border-gray-300 rounded-md w-full bg-gray-200'
+              placeholder='Enter a valid email address'
+            />
+          </div>
+          <div>
+            <label htmlFor='message' className='block text-lg font-medium text-gray-700'>Message</label>
+            <textarea
+              id='message'
+              name='message'
+              value={formData.message}
+              onChange={handleChange}
+              className='mt-1 p-2 border border-gray-300 rounded-md w-full bg-gray-200'
+              placeholder='Enter your message'
+              rows='4'
+            ></textarea>
+          </div>
+          <div>
+            <button
+              type='submit'
+              className='bg-customGreen text-white py-2 px-4 rounded-md w-full hover:bg-green-700 transition duration-300'
+            >
+              SUBMIT
+            </button>
+          </div>
+        </form>
+      </div>
+      </div>
+    </div>
+  );
 }
 
-export default ContactUs
+export default ContactUs;
+
